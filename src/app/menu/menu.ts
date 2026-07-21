3f86df6338ed35bc480b9b1526c05a379bbdb876
Pramod Rajane <pramod.rajane@gmail.com>
2026-07-21 23:20:45 +0530

    ngIf issue

--- src/app/menu/menu.ts
+++ src/app/menu/menu.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Authentication } from '../services/authentication';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
 })
@@ -16,6 +17,7 @@ export class Menu implements OnInit {
 
   ngOnInit(): void {
     this.isUserLoggedIn = this.authService.isUserLoggedIn();
+    console.log('User LoggedIn? == ' + this.isUserLoggedIn)
   }
   
   
